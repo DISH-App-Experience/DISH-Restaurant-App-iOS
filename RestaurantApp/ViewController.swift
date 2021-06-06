db03484e04162b8409e9c9463ec7ccbc9b25fe17
Jorge Zapata <jorgejaden@gmail.com>
2021-06-06 16:00:32 +0200

    Initial Commit

---
+++ RestaurantApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RestaurantApp
+//
+//  Created by JJ Zapata on 6/6/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
